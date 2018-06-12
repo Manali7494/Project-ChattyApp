b9337ddb647e92663473eea2c4e64b772c3994c0
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-12 14:54:29 -0600

    updated files with feature to add chat with constant user

---
+++ src/Message.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class Message extends Component{
+render(){
+  return(
+<main className="messages">
+         <div className="message">
+           <span className="message-username">{this.props.msgUsr}</span>
+           <span className="message-content">{this.props.msgContent}</span>
+         </div> 
+       </main>
+  )
+}
+}
+export default Message;
+
+// class IncomingMessage extends Component {
+//   // constructor(props) {
+//   //   super();
+//   //   this.state = {
+//   //     type: 'incomingMessage',
+//   //     content:
+//   //       'I won\'t be impressed with technology until I can download food.',
+//   //     username: 'Anonymous1'
+//   //   };
+//   // }
+//   render() {
+//     return (
+//       <main className="messages">
+//         <div className="message">
+//           <span className="message-username">Incoming Message</span>
+//           <span className="message-content">I won't be impressed with technology until I can download food</span>
+//         </div>
+//          <IncomingNotification /> 
+//       </main>
+//     );
+//   }
+// }
+
+// class IncomingNotification extends Component {
+//   constructor(props) {
+//     super();
+//     this.state = {
+//       type: 'incomingNotification',
+//       content: 'Anonymous1 changed their name to nomnom'
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div className="message system">
+//         Anonymous1 changed their name to nomnom.
+//       </div>
+//     );
+//   }
+// }
+
---
+++ src/MessageList.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Message from "./Message.jsx";
+class MessageList extends Component {
+  getList() {
+    let list = this.props.messages.map((item, index) => {
+    
+       return(<Message key={item.id} msgUsr={item.username} msgContent={item.content} />)
+    });
+    return <div> {list} </div>;
+  }
+
+  render() {
+    return <div> {this.getList()} </div>;
+  }
+}
+
+export default MessageList;
