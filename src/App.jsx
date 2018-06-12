d69a8f4915c4fccf2d931fe2eacb7e5319cf0011
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-12 14:51:35 -0600

    updated main component file time out methods and message

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
 import React, {Component} from 'react';
-
+import IncomingMessage from './Message.jsx';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currentUser:{name:"Bob"},
+      messages: [
+        {
+          id: 1,
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          id: 2,
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+  
+      ]
+  }
+}
+componentDidMount() {
+  console.log("componentDidMount <App />");
+  setTimeout(() => {
+    console.log("Simulating incoming message");
+    // Add a new message to the list of messages in the data store
+    let newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    let messages = this.state.messages.concat(newMessage)
+    // Update the state of the app component.
+    // Calling setState will trigger a call to render() in App and all child components.    
+    this.setState({messages: messages})
+  }, 3000);
+}
+
+addMessage(username, content){
+  let currentLength = this.state.messages.length;
+   let msgId = currentLength + 1;
+  let newMessage = {id: msgId, username: username, content: content};
+   let messages = this.state.messages.concat(newMessage);
+    this.setState({messages: messages});
+
+}
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">
+          Chatty </a>
+        </nav>
+        < MessageList messages={this.state.messages}/>
+        <ChatBar user={this.state.currentUser} addMsgFcn={this.addMessage.bind(this)}/>
+
+      </div>
     );
   }
 }
