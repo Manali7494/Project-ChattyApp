9c25b4651ad14f8fdbbf6d203ccdd4980e401e8c
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-13 22:00:58 -0600

    color addition on server

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -5,6 +5,7 @@ const WebSocket = require('ws')
 const SocketServer = WebSocket.Server;
 const uuidv4 = require('uuid/v4');
 const clients = [];
+const colors = ["#0000FF", "#DC143C", "#8B4513", "#2E8B57"];
 // Set the port to 3001
 const PORT = 3001;
 
@@ -22,6 +23,8 @@ const wss = new SocketServer({ server });
 
 wss.on('connection', function connection(ws) {
   console.log('client connected');
+  let randNum = Math.floor(Math.random()*4);
+  let selectedColor = colors[randNum];
   clients.push(ws);
   wss.clients.forEach(function each(client){
     if (client.readyState === WebSocket.OPEN){
@@ -37,7 +40,8 @@ wss.on('connection', function connection(ws) {
       type: "incomingMessage",
             id: uuidv4(),
       username: object.username,
-      content: object.content
+      content: object.content,
+      color: selectedColor
     }
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
@@ -49,7 +53,8 @@ wss.on('connection', function connection(ws) {
     let sendObject = {
       type: "incomingNotification",
       id: uuidv4(),
-      content: object.content
+      content: object.content,
+      color: selectedColor
     }
     wss.clients.forEach(function each(client){
       if (client.readyState === WebSocket.OPEN){
